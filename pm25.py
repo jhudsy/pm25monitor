f39874f7d6a77df7b42fc4a799c7bf55e87ae85b
Nir Oren <niroren@Nirs-MacBook-Pro.local>
2017-12-11 06:27:04 +0000

    more updates

--- pm25.py
+++ pm25.py
@@ -1,25 +1,63 @@
-import serial,time,datetime
-s=serial.Serial('/dev/ttyUSB0')
+import serial,time#,datetime
+from bisect import bisect_left
 
-while True:
-  b=s.read(20)
-  for i in range(len(b)):
-    x=int(b[i].encode('hex'),16)
-    if x==0xaa: #found header
-       break
+class serialWorker(Thread):
+  def __init__(self,port,datastore,lock):
+    Thread.__init__(self)
+    if port==None:
+      port="/dev/ttyUSB0"
+    self.s=serial.Serial(port)
+    self.datastore=datastore
+    self.lock=lock
+    self.start()
 
-  #get values
-  pm25l=int(b[i+2].encode('hex'),16)
-  pm25h=int(b[i+3].encode('hex'),16)
-  pm10l=int(b[i+4].encode('hex'),16)
-  pm10h=int(b[i+5].encode('hex'),16)
-  i1=int(b[i+6].encode('hex'),16)
-  i2=int(b[i+7].encode('hex'),16)
-  cs=int(b[i+8].encode('hex'),16)
-  checksum=(pm25l+pm25h+pm10l+pm10h+i1+i2)%256
-  if cs==checksum:
-    print datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S"), \
-          float(pm25h*256+pm25l)/10, \
-          float(pm10h*256+pm10l)/10
+  def readInt(self):
+    return int(self.s.read().encode('hex'),16)
 
-  time.sleep(1)
+  def readPM25PM10(self):
+    b=self.readInt()
+    while b!=0xaa:
+       b=self.readInt()
+    
+    pm25l=self.readInt()
+    pm25h=self.readInt()
+    pm10l=self.readInt()
+    pm10h=self.readInt()
+    i1=self.readInt()
+    i2=self.readInt()
+    cs=self.readInt()
+    checksum=(pm25l+pm25h+pm10l+pm10h+i1+i2)%256
+    if cs==checksum:
+      return [ float(pm25h*256+pm25l)/10,  float(pm10h*256+pm10l)/10 ]
+    return None
+
+  def run(self):
+    while True:
+      t=self.readPM25PM10()
+      if t!=None:
+        [p25,p10]=t
+        d=round(time.time())
+        self.lock.acquire()
+        self.datastore.append([d,p25,p10])
+        self.lock.release()
+      time.sleep(1)
+
+#returns a chart starting at startTime till the current time, with averages of timesize duration, e.g., starttime could be now-60 minutes, timesize could be 1 minute should return a minute average for the last hour
+def getChart(startTime,timesize,datastore):
+  while True:
+    s0=0.0
+    s1=0.0
+    startIndex=bisect_left(datastore,startTime)
+    endIndex=bisect_left(datastore,startTime+timesize)
+    if startIndex==endIndex:
+      break
+    for i in range(startIndex,endIndex):
+      s0+=datastore[i][1]
+      s1+=datastore[i][2]
+    s0/=(endIndex-startIndex+1)
+    s1/=(endIndex-startIndex+1)
+
+
+
+
+#      print datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S"), \

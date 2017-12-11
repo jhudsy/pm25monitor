162aadfaeae041d7242b1ec12724e58a39eb6d48
Nir Oren <niroren@Nirs-MacBook-Pro.local>
2017-12-11 20:20:20 +0000

    removed binary search

--- util.py
+++ util.py
@@ -15,29 +15,29 @@ def bisect_left(datastore,value):
    return lo
 
 #datastore is a [date,pm25,pm10] triple in each entry, where date is number of millis from epoch
-def getPMAverages(startTime,timesize,datastore):
+def getPMAverages(startIndex,timesize,datastore):
     s0=0.0
     s1=0.0
-    startIndex=bisect_left(datastore,startTime)
-    endIndex=bisect_left(datastore,startTime+timesize)-1 #see note above
-    if startIndex>endIndex or datastore[startIndex][0]<startTime:
-      return None
-    for i in range(startIndex,endIndex):
-      s0+=datastore[i][1]
-      s1+=datastore[i][2]
-    s0/=(endIndex-startIndex+1)
-    s1/=(endIndex-startIndex+1)
+    i=startIndex
+    while i<len(datastore) and datastore[i][0]<startTime+timesize:
+       s0+=datastore[i][1]
+       s1+=datastore[i][2]
+       i+=1
+    if i==startIndex:
+      return(i,None)
+    s0/=(i-startIndex)
+    s1/=(i-srartIndex)
 
-    return [startTime+timesize//2,s0,s1]
+    return (i,[startTime+timesize//2,s0,s1])
 
 #returns a chart starting at startTime till the current time, with averages of timesize duration, e.g., starttime could be now-60 minutes, timesize could be 1 minute should return a minute average for the last hour
 def makeChartData(startTime,timesize,datastore):
   data=[]
-  while startTime<datastore[-1][0]:
-    a=getPMAverages(startTime,timesize,datastore)
+  index=bisect_left(datastore,startTime)
+  while index<len(datastore)-1
+    (index,a)=getPMAverages(index,timesize,datastore)
     if not a==None:
       data.append(a)
-    startTime+=timesize
   return data[:-1] #drop last datapoint in case it's incomplete  
 
 def makePGChart(startTime,timesize,datastore):

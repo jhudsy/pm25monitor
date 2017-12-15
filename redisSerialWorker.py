3dad1475c87c58ba6fc9ad6068b48ca3d0e4b232
jhudsy <jhudsy@github.com>
2017-12-15 17:01:08 +0000

    first addition

---
+++ redisSerialWorker.py
@@ -0,0 +1,50 @@
+import serial,time,redis
+
+"""store readings every minute"""
+__SAMPLINGINTERVAL=60
+"""serial port device from which to read"""
+__PORT="/dev/ttyUSB0"
+__serialport=serial.Serial(__PORT)
+"""redis key for sorted set"""
+__SSKEY="sensor1ts" 
+"""redis key for current reading"""
+__KEY="sensor1Current"
+
+def readInt():
+    i = int(__serialport.read().encode('hex'),16)
+    return i
+
+  #NB: return is p2.5,p10
+def readPM25PM10():
+    b=readInt()
+    while b!=0xaa:
+       b=readInt()
+    readInt() #read dummy
+    pm25l=readInt()
+    pm25h=readInt()
+    pm10l=readInt()
+    pm10h=readInt()
+    i1=readInt()
+    i2=readInt()
+    cs=readInt()
+    checksum=(pm25l+pm25h+pm10l+pm10h+i1+i2)%256
+    if cs==checksum:
+      return [ (pm25h*256+pm25l)/10,  (pm10h*256+pm10l)/10 ]
+    return None
+
+#####################################
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+lastwrite=round(time.time()) #time last reading was stored
+readings=[] #the readings
+while True:
+      t=self.readPM25PM10()
+      d=round(time.time())
+      if t!=None:
+        readings.append(t)      
+        r.set(__KEY,t)
+      if d-lastread>=__SAMPLINGINTERVAL:
+              lastwrite=d
+              toRedis=np.sum(readings,axis=0)/len(readings)
+              r.zadd(__SSKEY,(d-__SAMPLINGINTERVAL)//2,toRedis.tolist())
+              readings=[]
+      time.sleep(1)

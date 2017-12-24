5b4a4e8bc3c921d326e274897aa1424894562bc7
Nir Oren <niroren@Nirs-MacBook-Pro.local>
2017-12-24 14:13:13 +0000

    more progress

--- web/pm25.py
+++ web/pm25.py
@@ -1,19 +1,27 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS, cross_origin
 import redis
+import logging
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+logging.getLogger('flask_cors').level = logging.DEBUG
 
 application=Flask(__name__)
 CORS(application)
 
 @application.route("/")
 def display():
-    return application.send_static_file('pm25.html')
+    r=application.send_static_file('pm25.html')
+    r.headers.add('Access-Control-Allow-Origin', '*')
+    return r
 
 @application.route("/getData",methods=['POST','GET'])
 def data():
   start=request.form['starttime']
   end=request.form['endtime']
   sensor=request.form['sensor']
+  print jsonify(r.zrangebyscore(sensor,int(start),int(end)))
   return jsonify(r.zrangebyscore(sensor,int(start),int(end)))
+
+application.run(host='0.0.0.0')
